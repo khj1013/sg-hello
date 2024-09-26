df0dc90e312b649f0d85821c153871f5b59cb23d
heejin <khj1013@skuniv.ac.kr>
2024-09-26 13:33:16 +0900

    sg-hello first commit

---
+++ src/test/java/com/junetan2ee/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junetan2ee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
